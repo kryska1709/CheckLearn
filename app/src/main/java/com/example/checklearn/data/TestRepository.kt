bfcdcf4fb6ab2949964079311014da20f69cf16f
Your Name <you@example.com>
2026-02-20 21:27:53 +0300

    update history screen, added save data

--- app/src/main/java/com/example/checklearn/data/TestRepository.kt
+++ app/src/main/java/com/example/checklearn/data/TestRepository.kt
@@ -1,9 +1,13 @@
 package com.example.checklearn.data
 
 import com.example.checklearn.model.TestResult
+import com.example.checklearn.model.UserProfile
 import com.example.checklearn.view.TestScreen
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 
 class TestRepository {
@@ -19,12 +23,20 @@ class TestRepository {
             .collection("tests")
             .add(result).await()
     }
-    suspend fun getTestsHistory(): List<TestResult>{
-        val userId = auth.currentUser?.uid ?: return emptyList()
-        return firestore
-            .collection("users")
-            .document(userId)
-            .collection("tests")
-            .get().await().toObjects(TestResult::class.java)
+    fun getTestHistory(): Flow<List<TestResult>> = callbackFlow{
+        val userId = auth.currentUser?.uid?: run {
+            trySend(emptyList())
+            close()
+            return@callbackFlow
+        }
+        val listener = firestore.collection("users").document(userId).collection("tests")
+            .addSnapshotListener { snapshot,error ->
+                if (error!=null){
+                    close(error)
+                    return@addSnapshotListener
+                }
+                trySend(snapshot?.toObjects(TestResult::class.java) ?: emptyList())
+            }
+        awaitClose { listener.remove() }
     }
 }
--- app/src/main/java/com/example/checklearn/viewmodel/HistoryViewModel.kt
+++ app/src/main/java/com/example/checklearn/viewmodel/HistoryViewModel.kt
@@ -1,29 +1,25 @@
 package com.example.checklearn.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.checklearn.data.TestRepository
 import com.example.checklearn.model.HistoryUIState
 import com.example.checklearn.model.TestResult
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 
 class HistoryViewModel(
     private val testRepository: TestRepository = TestRepository(),
     private val authViewModel: AuthViewModel,
     private val profileViewModel: ProfileViewModel
 ): ViewModel() {
-    private val _history = MutableStateFlow<List<TestResult>>(emptyList())
-    val history = _history.asStateFlow()
+    val history:StateFlow<List<TestResult>> = testRepository.getTestHistory()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000),emptyList())
 
     val uiState: StateFlow<HistoryUIState> =
-        combine(authViewModel.user, profileViewModel.profile, _history){user,profile,history ->
+        combine(authViewModel.user, profileViewModel.profile, history){user,profile,history ->
             when{
                 user==null -> HistoryUIState.Unauthorized
                 profile==null -> HistoryUIState.NeedProfileInfo
@@ -31,16 +27,4 @@ class HistoryViewModel(
             }
         }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), HistoryUIState.Unauthorized)
 
-    init {
-        getHistory()
-    }
-    fun getHistory(){
-        viewModelScope.launch {
-            try {
-                _history.value = testRepository.getTestsHistory()
-            } catch (e: Exception){
-                Log.e("ErorrGetHistory", e.message.toString())
-            }
-        }
-    }
 }
